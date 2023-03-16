aa23ffb97549ac117f02b8f01987880b2423e8a2
Thhommy <thhommy@hotmail.com>
2023-03-16 16:03:16 +0100

    players now can add their own markings in order

--- javascript.js
+++ javascript.js
@@ -3,14 +3,14 @@ const cell = document.querySelectorAll(".cell");
 
 const GameBoard = (() => {
     let board = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
     ];
 
     const printBoard = () => { 
         i = 0;
-        cell.forEach(element => {
+         cell.forEach(element => {
             if (i <= 2){
             element.textContent = board[0][i];
             }else if (i > 2 && i <= 5) {
@@ -19,32 +19,90 @@ const GameBoard = (() => {
                 element.textContent = board[2][i - 6];
             }
             i += 1;
-        });
+        }); 
     };
 
-    return {board, printBoard};
+    const changeCell = () => {
+        cell.forEach(element => {
+
+            element.addEventListener('click', function(e) {
+                    if (e.target.innerHTML === ''){
+                        if (e.target.classList.contains('cell1')){
+                            GameBoard.board[0][0] = Gameplay.playerPick();
+                            console.log(GameBoard.board)
+                            GameBoard.printBoard();
+                        }else if (e.target.classList.contains('cell2')){
+                            GameBoard.board[0][1] = Gameplay.playerPick();
+                            console.log(GameBoard.board)
+                            GameBoard.printBoard();
+                        }else if (e.target.classList.contains('cell3')){
+                            GameBoard.board[0][2] = Gameplay.playerPick();
+                            console.log(GameBoard.board)
+                            GameBoard.printBoard();
+                        }else if (e.target.classList.contains('cell4')){
+                            GameBoard.board[1][0] = Gameplay.playerPick();
+                            console.log(GameBoard.board)
+                            GameBoard.printBoard();
+                        }else if (e.target.classList.contains('cell5')){
+                            GameBoard.board[1][1] = Gameplay.playerPick();
+                            console.log(GameBoard.board)
+                            GameBoard.printBoard();
+                        }else if (e.target.classList.contains('cell6')){
+                            GameBoard.board[1][2] = Gameplay.playerPick();
+                            console.log(GameBoard.board)
+                            GameBoard.printBoard();
+                        }else if (e.target.classList.contains('cell7')){
+                            GameBoard.board[2][0] = Gameplay.playerPick();
+                            console.log(GameBoard.board)
+                            GameBoard.printBoard();
+                        }else if (e.target.classList.contains('cell8')){
+                            GameBoard.board[2][1] = Gameplay.playerPick();
+                            console.log(GameBoard.board)
+                            GameBoard.printBoard();
+                        }else if (e.target.classList.contains('cell9')){
+                            GameBoard.board[2][2] = Gameplay.playerPick();
+                            console.log(GameBoard.board)
+                            GameBoard.printBoard();
+                        }
+                }
+              });
+        });
+    }; 
+
+    return {board, printBoard, changeCell};
 })();
 
 
-const Player = (name, sign) => {
-    const changeCell = () => {
-    cell.forEach(element => {
-        element.addEventListener('click', () => {
-            element.style.backgroundColor = 'red';
-        })
-    })};
-    return {name, sign, changeCell};
+const Player = (name, sign, turn) => {
+   
+    return {name, sign, turn};
 };
 
+const playerOne = Player('first', "X", true);
+const playerTwo = Player('second', 'O', true);
+
+
 const Gameplay = (() => {
+    const playerPick = () => {
+        if(playerOne.turn){
+            return playerOne.sign;
+        }else if(playerTwo.turn) {
+            return playerTwo.sign;
+        }
+    }
 
-})()
+    const gameFlow = () => {
+        cell.forEach(element => {
+            element.addEventListener('click', () => {
+                playerOne.turn = !playerOne.turn;
+            })
+        });
+    }
+
+   return {playerPick, gameFlow}
+})();
 
 
-GameBoard.printBoard();
-
-const playerOne = Player('first', "X");
-
-playerOne.changeCell();
-
-
+Gameplay.gameFlow()
+GameBoard.printBoard()
+GameBoard.changeCell()
